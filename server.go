a0bf3063d667202f17c02324fafdf8365c42ab81
FIT IPC <s15a1302@bene.fit.ac.jp>
2017-06-23 15:49:37 +0900

    add_get_server_info

--- server.go
+++ server.go
@@ -3,7 +3,18 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
+
+func getMyIpAddress(w http.ResponseWriter, r *http.Request)  {
+	url := "http://ipinfo.io"
+
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(byteArray)) // htmlをstringで取得
+}
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, World")
@@ -12,5 +23,6 @@ func helloWorld(w http.ResponseWriter, r *http.Request){
 func main() {
 	fmt.Print(" \n ---Hello World---\n")
 	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/ip", helloWorld)
 	http.ListenAndServe(":8080", nil)
 }
